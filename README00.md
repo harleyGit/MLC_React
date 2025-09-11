8649c8a93c627375da3a8d27a4aa63c11d18f986
GangHuang <harleysor@qq.com>
2025-09-11 20:13:26 +0800

    feat: 🍒 环境区分

--- README00.md
+++ README00.md
@@ -1,2 +1,17 @@
+<!--
+ * @Author: GangHuang harleysor@qq.com
+ * @Date: 2025-09-11 09:18:34
+ * @LastEditors: GangHuang harleysor@qq.com
+ * @LastEditTime: 2025-09-11 19:49:05
+ * @FilePath: /MLC_React/README00.md
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
 # MLC_React
 React开发、项目总结、平时使用组件，自己封装的轮子
+
+- **运行**
+  - npm run dev → 使用 .env.debug
+
+  - npm run build:pre → 使用 .env.pre
+
+  - npm run build:release → 使用 .env.release
--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,11 @@
+/*
+ * @Author: GangHuang harleysor@qq.com
+ * @Date: 2025-09-11 09:31:27
+ * @LastEditors: GangHuang harleysor@qq.com
+ * @LastEditTime: 2025-09-11 19:50:40
+ * @FilePath: /MLC_React/src/App.jsx
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -8,6 +16,11 @@ function App() {
 
   return (
     <>
+    <div>
+      <h1>Hello React</h1>
+      <p>当前环境: {import.meta.env.VITE_APP_ENV}</p>
+      <p>API 地址: {import.meta.env.VITE_API_URL}</p>
+    </div>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
